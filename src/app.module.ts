0629bacd89d68db695541f26f3f18c30ba54fa6c
Swag-blips <coderblip@gmail.com>
2025-05-17 17:03:01 +0100

    feat:setup owner folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'; 
+import { OwnerModule } from './owner/owner.module';
 
 @Module({
-  imports: [],
+  imports: [OwnerModule],
   controllers: [],
   providers: [],
 })
